e709c05cc161e81ccac7b1e05ce9a3a6fd801408
Jake Schurch <jakeschurch@gmail.com>
2025-01-03 12:45:14 -0500

    update k9s config

--- modules/programs/k9s/default.nix
+++ modules/programs/k9s/default.nix
@@ -1,7 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = with pkgs; [
     k9s
   ];
+
+  home.sessionVariables."K9S_CONFIG_DIR" = "${config.xdg.configHome}/k9s";
 
   xdg.configFile."k9s" = {
     recursive = true;
