f382fac6338871c81042f00f45837999cc2c09bb
Jake Schurch <jakeschurch@gmail.com>
2025-08-10 12:46:10 -0400

    fixup

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,23 +1,19 @@
-{ _, ... }:
+{ pkgs, ... }:
 {
-  perSystem =
-    { pkgs', ... }:
-    {
-      home.packages = with pkgs'; [
-        obs-studio
-        nixgl.nixGLIntel
-        steam
-        rofi
-        spotify
-        docker
-        wmctrl
-        xbanish
-        playerctl
-        xbindkeys
-        xautolock
-        yubikey-manager
-        ethtool
-        # xev-1.2.4
-      ];
-    };
+  home.packages = with pkgs; [
+    obs-studio
+    nixgl.nixGLIntel
+    steam
+    rofi
+    spotify
+    docker
+    wmctrl
+    xbanish
+    playerctl
+    xbindkeys
+    xautolock
+    yubikey-manager
+    ethtool
+    # xev-1.2.4
+  ];
 }
