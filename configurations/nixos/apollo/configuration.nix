49652256bf497261747e2d11b26097192a50902f
Jake Schurch <jakeschurch@gmail.com>
2025-09-05 18:33:06 -0400

    let it pick the font

--- configurations/nixos/apollo/configuration.nix
+++ configurations/nixos/apollo/configuration.nix
@@ -102,7 +102,7 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
-    font = "Lat2-Terminus16";
+    font = null;
     useXkbConfig = true; # use xkb.options in tty.
   };
 
