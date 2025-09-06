4cdaabbe6ad9e25b436e118fe47a0beb721e5441
Jake Schurch <jakeschurch@gmail.com>
2025-09-06 11:47:59 -0400

    test

--- starfield/flake.nix
+++ starfield/flake.nix
@@ -16,33 +16,21 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        libPath =
+          with pkgs;
+          lib.makeLibraryPath [
+            libGL
+            libxkbcommon
+            wayland
+            vulkan-loader
+          ];
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.rust
-            pkgs.wayland
-            pkgs.libxkbcommon
-            pkgs.vulkan-loader
-            pkgs.vulkan-tools
-            pkgs.nvidia_x11 # Nvidia proprietary driver
-          ];
-
-          shellHook = ''
-            # Make Wayland backend work
-            export WINIT_UNIX_BACKEND=wayland
-
-            # Make wgpu find Vulkan and Nvidia GPU
-            export WGPU_BACKEND=vulkan
-            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.nvidia_x11}/lib
-            export VK_ICD_FILENAMES=/run/opengl-driver/etc/vulkan/icd.d/nvidia_icd.json
-            export VK_LAYER_PATH=/run/opengl-driver/etc/vulkan/explicit_layer.d
-
-            # Optional: show debug info
-            echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
-            echo "VK_ICD_FILENAMES=$VK_ICD_FILENAMES"
-          '';
-        };
+        devShell =
+          with pkgs;
+          mkShell {
+            LD_LIBRARY_PATH = libPath;
+          };
       }
     );
 }
