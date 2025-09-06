5ee54ea963f15d3d4a6196192f4e0cf5b8cdcf93
Jake Schurch <jakeschurch@gmail.com>
2025-09-05 20:50:36 -0400

    save

--- configurations/nixos/apollo/default.nix
+++ configurations/nixos/apollo/default.nix
@@ -13,9 +13,6 @@ in
     enable = true;
     xwayland.enable = true;
     withUWSM = true;
-
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
   };
 
   environment.variables.NIXOS_OZONE_WL = "1";
