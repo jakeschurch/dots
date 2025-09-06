7b16df10f1973f39dd6102b917e0b5cbe784fcb4
Jake Schurch <jakeschurch@gmail.com>
2025-09-05 20:08:55 -0400

    save

--- configurations/nixos/apollo/default.nix
+++ configurations/nixos/apollo/default.nix
@@ -13,10 +13,6 @@ in
     enable = true;
     xwayland.enable = true;
     withUWSM = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   environment.variables.NIXOS_OZONE_WL = "1";
