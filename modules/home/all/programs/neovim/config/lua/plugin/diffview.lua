13acaf5fc426ae4468f937cdf3b1c0a16328f596
Jake Schurch <jakeschurch@gmail.com>
2026-02-03 09:37:21 -0800

    update diffview setup

--- modules/home/all/programs/neovim/config/lua/plugin/diffview.lua
+++ modules/home/all/programs/neovim/config/lua/plugin/diffview.lua
@@ -1,4 +1,6 @@
-require("diffview").setup({
+local diffview = require("diffview")
+
+diffview.setup({
   diff_binaries = false,
   enhanced_diff_hl = true,
   use_icons = true,
@@ -7,28 +9,37 @@ require("diffview").setup({
     folder_open = "",
     folder_empty = "ﰊ",
   },
+  default_args = {
+    DiffviewOpen = { "HEAD" },
+    DiffviewFileHistory = {},
+  },
   view = {
-    merge_tool = {
+    default = {
       layout = "diff3_mixed",
       disable_diagnostics = true,
       winbar_info = false,
     },
   },
+  keymaps = {
+    view = {
+      ["[h"] = diffview.config.actions.select_next_entry,
+      ["]h"] = diffview.config.actions.select_prev_entry,
+      ["q"] = diffview.config.actions.close,
+    }
+  }
 })
 
-local function smart_git_diff()
-  local ft = vim.bo.filetype
-  if ft == "fugitive" or ft == "git" or ft == "gitcommit" then
-    vim.cmd("DiffviewOpen")
-  else
-    -- fallback to diff current file vs HEAD
-    vim.cmd("DiffviewOpen HEAD -- " .. vim.fn.expand("%:p"))
-  end
-end
+local maps = {
+  { "<leader>ho", function() require("diffview.config").actions.conflict_choose("ours") end,   desc = "Choose Ours" },
+  { "<leader>ht", function() require("diffview.config").actions.conflict_choose("theirs") end, desc = "Choose Theirs" },
+  { "<leader>hr", function() require("diffview.config").actions.conflict_choose("none") end,   desc = "Choose Base" },
+}
+
+require("which-key").add(maps, { mode = "n" })
 
 vim.keymap.set(
   "n",
   "<leader>dv",
-  smart_git_diff,
+  function() diffview.open() end,
   { desc = "Smart Git Diff (Diffview or file)" }
 )
